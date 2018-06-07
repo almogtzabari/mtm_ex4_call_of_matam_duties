35723994741adbd379efb75814838818c10c936f
Ariel Kfir <Arielkfir@campus.technion.ac.il>
2018-06-08 01:11:55 +0200

    f

--- Game.cpp
+++ Game.cpp
@@ -5,6 +5,15 @@
 #include "Game.h"
 #include "Player.h"
 
-Game::Game(int maxPlayer) : max_players(maxPlayer){
-    player_array= new Player [maxPlayer];
+Game::Game(int maxPlayer) : max_players(maxPlayer),
+                            player_array( new Player[maxPlayer]){
+}
+
+Game::~Game() {
+    delete[] player_array;
+}
+
+GameStatus Game::addPlayer(const char *playerName, const char *weaponName,
+                           Target target, int hit_strenth) {
+    
 }
--- Game.h
+++ Game.h
@@ -7,12 +7,30 @@
 
 #include "Player.h"
 
+enum GameStatus
+{
+    INVALID_PARAM,
+    NAME_ALREADY_EXSISTS,
+    GAME_FULL,
+    NAME_DOES_NOT_EXIST,
+    FIGHT_FAILED,
+    SUCCESS
+};
+
+
+
 class Game {
     int max_players;
     Player* player_array;
 public:
     Game(int maxPlayer);
+    ~Game();
+    GameStatus addPlayer(const char* playerName,const char* weaponName,
+    Target target,int hit_strenth);
 };
+
+
+
 
 
 #endif //MTM_EX4_CALL_OF_MATAM_DUTIES_GAME_H
